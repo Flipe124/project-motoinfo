d5821fa5d2ead9f9ff57d791073125b98dcfbb7a
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-12-27 11:26:08 -0300

    feat: adding new function

---
+++ js/fill.js
@@ -0,0 +1,32 @@
+function fillCard(src_img, brand, model, info, cc, power, torque, fuel, fuel_capacity, oil_capacity, gear, seat_height, soil_height) {
+    var elementSectionSearchResult = document.querySelector("#section-search-result");
+    const UNIT_TORQUE = "kgfm";
+    const UNIT_POWER = "cv";
+
+    elementSectionSearchResult.innerHTML +=
+        `<div class="result-card" data-srcimg="${src_img}" data-brand="${brand}"
+            data-name="${model}" data-info="${info}" data-cc="${cc}" data-power="${power}" data-torque="${torque}"
+            data-fuel="${fuel}" data-fuel_capacity="${fuel_capacity}" data-oil_capacity="${oil_capacity}" data-gear="${gear}"
+            data-seat_height="${seat_height}" data-soil_height="${soil_height}">
+            <div class="moto-img">
+                <img src="${src_img}" alt="Imagem da ${brand} ${model}">
+            </div>
+            <div class="data">
+                <span class="brand">${brand}</span>
+                <span class="name">${model}</span>
+                <p class="info">
+                    ${info}
+                </p>
+                <div class="resume">
+                    <div class="power">
+                        <i class="fa-solid fa-horse-head"></i>
+                        <span>${power + UNIT_POWER}</span>
+                    </div>
+                    <div class="torque">
+                        <i class="fa-solid fa-wrench"></i>
+                        <span>${torque + UNIT_TORQUE}</span>
+                    </div>
+                </div>
+            </div>
+        </div>`;
+}
