9ae2740e92005ba47632c6ec486158c4240a48d1
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-12-28 17:18:56 -0300

    feat: adding new  event to navbar

--- JS/navbar.js
+++ JS/navbar.js
@@ -2,3 +2,13 @@ const buttonMenu = document.querySelector(".menu-button");
 const navbar = document.querySelector(".navbar");
 
 buttonMenu.addEventListener("click", () => navbar.classList.toggle("active"));
+
+window.addEventListener('scroll', function () {
+    var navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
