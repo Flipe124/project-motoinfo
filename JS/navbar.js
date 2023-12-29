04af8eba7f117d330dd70434b651258f5f4c341c
Felipe Oliveira <flipeoliveira007@gmail.com>
2023-12-29 10:40:28 -0300

    feat: adding new function to scrollTo control

--- JS/navbar.js
+++ JS/navbar.js
@@ -12,3 +12,24 @@ window.addEventListener('scroll', function () {
         navbar.classList.remove('scrolled');
     }
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarHeight = document.querySelector('.navbar').offsetHeight;
+    var links = document.querySelectorAll('.menu-list a');
+    
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute('href').substring(1);
+            var targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop - navbarHeight - 10,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
