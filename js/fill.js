3e828f7fc7c38840b69c1cc69eae897a0a2c823d
Felipe Oliveira <85032728+Flipe124@users.noreply.github.com>
2024-02-11 19:42:02 -0300

    feat: changing to receive constructor

--- js/fill.js
+++ js/fill.js
@@ -1,4 +1,5 @@
-function fillCard(src_img, brand, model, info, cc, power, torque, fuel, fuel_capacity, oil_capacity, gear, seat_height, soil_height, max_speed) {
+// function fillCard(src_img, brand, model, info, cc, power, torque, fuel, fuel_capacity, oil_capacity, gear, seat_height, soil_height, max_speed) {
+function fillCard(motorcycle) {
     var elementSectionSearchResult = document.querySelector("#section-search-result");
     const UNIT_TORQUE = "kgfm";
     const UNIT_POWER = "cv";
@@ -6,40 +7,40 @@ function fillCard(src_img, brand, model, info, cc, power, torque, fuel, fuel_cap
     const UNIT_SPEED = "Km/h";
 
     elementSectionSearchResult.innerHTML +=
-        `<div class="result-card" data-srcimg="${src_img}" data-brand="${brand}"
-            data-name="${model}" data-info="${info}" data-cc="${cc}" data-power="${power}" data-torque="${torque}"
-            data-fuel="${fuel}" data-fuel_capacity="${fuel_capacity}" data-oil_capacity="${oil_capacity}" data-gear="${gear}"
-            data-seat_height="${seat_height}" data-soil_height="${soil_height}">
+        `<div class="result-card" data-srcimg="${motorcycle.src_img}" data-brand="${motorcycle.brand}"
+            data-name="${motorcycle.gearmodel}" data-info="${motorcycle.info}" data-cc="${motorcycle.cc}" data-power="${motorcycle.power}" data-torque="${motorcycle.torque}"
+            data-fuel="${motorcycle.fuel}" data-fuel_capacity="${motorcycle.fuel_capacity}" data-oil_capacity="${motorcycle.oil_capacity}" data-gear="${motorcycle.gear}"
+            data-seat_height="${motorcycle.seat_height}" data-soil_height="${motorcycle.soil_height}">
             <div class="moto-img">
-                <img src="${src_img}" alt="Imagem da ${brand} ${model}">
+                <img src="${motorcycle.src_img}" alt="Imagem da ${motorcycle.brand} ${motorcycle.model}">
             </div>
             <div class="data">
-                <span class="brand">${brand}</span>
-                <span class="name">${model}</span>
+                <span class="brand">${motorcycle.brand}</span>
+                <span class="name">${motorcycle.model}</span>
                 <p class="info">
-                    ${info}
+                    ${motorcycle.info}
                 </p>
                 <div class="resume">
                     <!--<div class="">
                         <i class="fa-solid fa-database"></i>
-                        <span>${cc}cc</span>
+                        <span>${motorcycle.cc}cc</span>
                     </div>-->
                     <div class="power">
                         <i class="fa-solid fa-horse-head"></i>
-                        <span>${power + UNIT_POWER}</span>
+                        <span>${motorcycle.power + UNIT_POWER}</span>
                     </div>
                     <div class="torque">
                         <i class="fa-solid fa-wrench"></i>
-                        <span>${torque + UNIT_TORQUE}</span>
+                        <span>${motorcycle.torque + UNIT_TORQUE}</span>
                     </div>
                     <div class="">
                         <i class="fa-solid fa-gauge-high"></i>
-                        <span>${max_speed ? max_speed + UNIT_SPEED : "Indefinido"}</span>
+                        <span>${motorcycle.max_speed ? motorcycle.max_speed + UNIT_SPEED : "Indefinido"}</span>
                     </div>
                     <div class="">
                     <i class="fa-solid fa-battery-full"></i>
                     <!-- <i class="fa-solid fa-gas-pump"></i> -->
-                        <span>${fuel_capacity + UNIT_VOLUME}</span>
+                        <span>${motorcycle.fuel_capacity + UNIT_VOLUME}</span>
                     </div>
                     <div class="max-width">
                         <span><i class="fa-solid fa-gas-pump"></i> 25km/L</span>
