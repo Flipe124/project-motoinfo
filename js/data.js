b8d17d8b51ffca536337ea7648fce99ee7a08f1d
Felipe Oliveira <85032728+Flipe124@users.noreply.github.com>
2024-02-16 22:46:52 -0300

    feat: modifying info

--- js/data.js
+++ js/data.js
@@ -43,7 +43,7 @@ motorcycleInfo.push(new Motorcycle("./img/kawasaki-ninja-300.png", "Kawasaki", "
 motorcycleInfo.push(new Motorcycle("./img/kawasaki-z-400.png", "Kawasaki", "Z 400", "Moto naked de 296cc, Dois cilindros.", 399, 48, 3.9, "Gasolina e/ou Etanol", 14, 2.3, 6, 785, "", 180, "Líquida"));
 motorcycleInfo.push(new Motorcycle("./img/honda-cb-twister.png", "Honda", "Cb Twister", "Moto naked de 250cc, monocilindro.", 249.5, 22.6, 2.28, "Gasolina e/ou Etanol", 16.5, 1.8, 6, 784, "", 139, "A ar"));
 motorcycleInfo.push(new Motorcycle("./img/kawasaki-versys-650.png", "Kawasaki", "Versys 650", "Moto Sport Touring de 649cc, Dois cilindros.", 649, 59.4, 6.5, "Gasolina e/ou Etanol", 19, 2.4, 6, 840, "", 205, "Líquida", 25));
-motorcycleInfo.push(new Motorcycle("./img/honda-adv-150.png", "Honda", "ADV 150", "Moto Scooter de 149cc, monocilindro.", 149.3, 13.2, 1.38, "Gasolina e/ou Etanol", 8, 0.9, "CVT", 795, "", 115, "A ar", 27.5));
+motorcycleInfo.push(new Motorcycle("./img/honda-adv-150.png", "Honda", "ADV 150", "Moto Scooter de 149cc, monocilindro.", 149.3, 13.2, 1.38, "Gasolina e/ou Etanol", 8, 0.9, "CVT", 795, "", 115, "A ar", 50.9));
 
 motorcycleInfo.forEach(Motorcycle => {
     fillCard(Motorcycle);
